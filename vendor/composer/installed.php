84e6ffd98dae4511d6e2eea2a6a62ca5f13ffba4
Byron <bajcucg@miumg.edu.gt>
2023-09-29 15:02:24 -0600

    se corrige composer en proyecto

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,31 +1,31 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '5eda95d0ff0c27d134f43a6fad765c2ae3e52df5',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
+        'reference' => '2794bd8eea9050d31e379968f8125ee241a67587',
+        'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '5eda95d0ff0c27d134f43a6fad765c2ae3e52df5',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
+            'reference' => '2794bd8eea9050d31e379968f8125ee241a67587',
             'dev_requirement' => false,
         ),
         'composer/installers' => array(
             'pretty_version' => 'v1.12.0',
             'version' => '1.12.0.0',
-            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/./installers',
             'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
             'dev_requirement' => false,
         ),
         'roundcube/plugin-installer' => array(
